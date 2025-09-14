f1226431542d9c6e96a18ad0731c3831fa719c1f
Aditya122221 <adityakumar122221@gmail.com>
2025-09-14 11:04:01 +0530

    main route added

--- server/index.js
+++ server/index.js
@@ -44,6 +44,10 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send("Server running")
+})
+
 // Health check endpoint
 app.get('/health', (req, res) => {
     res.json({ status: 'ok' });
